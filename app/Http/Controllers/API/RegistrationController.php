759b3af48146ea67339f77130f0af99eaef51dfd
Haseebkasoori <m.h.kasoori@gmail.com>
2021-11-22 13:14:48 +0500

    testing

--- app/Http/Controllers/API/RegistrationController.php
+++ app/Http/Controllers/API/RegistrationController.php
@@ -7,28 +7,30 @@ use App\Http\Requests\RegistrationRequest;
 use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\Controller;
 use App\Mail\RegistrationMail;
-use App\Models\User; 
+use App\Models\User;
 
 
 class RegistrationController extends Controller
 {
-    /** 
-     * Register api 
-     * 
-     * @return \Illuminate\Http\Response 
-     */ 
+
+
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function register(RegistrationRequest $request)
-    {   
+    {
             $input=$request->validated();
-            $input['password'] = bcrypt($input['password']); 
+            $input['password'] = bcrypt($input['password']);
             $verification_token=md5($input['name']);  //for encoding
             $gender = $input['gender'];
             $input['verification_token']=$verification_token;
-            $file_name=null; 
+            $file_name=null;
             // converting base64 decoded image to simple image if exist
             if (!empty($input['profile_image'])) {
                 // upload Attachment
-                $destinationPath = storage_path('\app\public\users\\'); 
+                $destinationPath = storage_path('\app\public\users\\');
                 $input_type_aux = explode("/", $input['profile_image']['mime']);
                 $attachment_type=$input_type_aux[0];
                 $attachment_extention=$input_type_aux[1];
@@ -39,14 +41,14 @@ class RegistrationController extends Controller
                 file_put_contents($file, $image_base64);
             }
             $input['profile_image'] = $file_name;
-            $user = User::create($input); 
+            $user = User::create($input);
             $data['verification_token']=$verification_token;
             $data['email']=$input['email'];
             $data['gender'] = $input['gender'];
             //for generate link in URL
             $data['link']=url('api/emailConfirmation/'.$input['email'].'/'.$verification_token);
             Mail::to($input['email'])->send(new testMail($data));
-            
+
             // data creation for response
             $response_data['data']=Null;
             $response_data['message']=strtoupper($user->user_name).', Please check your mail ('.$user->email.') for Email Varification';
@@ -72,7 +74,7 @@ class RegistrationController extends Controller
             }
         }
         else{
-            
+
             $data['error']='Email not Register!';
             $data['message']="Someting went Worng";
             return response()->error($data,404);
